3434f66ee655ea906697b01a675f7da6fee98981
ethanJlab <ethanjanicki89@gmail.com>
2023-03-10 13:05:03 -0500

    ADD csv printing features

--- dp_fib.cpp
+++ dp_fib.cpp
@@ -2,6 +2,54 @@
 
 #include <iostream>
 #include <chrono>
+#include <iostream>
+#include <cassert>
+#include <iomanip>
+#include <ctime>
+#include <fstream>
+
+void testingSuite(long long n);
+long long int fibonacci(int n);
+
+bool printTOCSV = true;
+long long maxFib = 90;
+
+using namespace std;
+
+
+
+int main () 
+{
+    int n;
+
+    // Currently only supports up to the 92nd term before going out of bounds
+    std::cout << "Enter number of terms: "; std::cin >>n;
+
+    auto start = std::chrono::high_resolution_clock::now(); // Starts clock
+
+    std::cout << n<<" Fibonacci Term: "<<fibonacci(n)<<std::endl;
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); // Time elapsed
+
+    std::cout << "Program finished in " << duration.count() << " microseconds" << std::endl;
+
+    if (printTOCSV) {
+        std::ofstream myfile;
+        myfile.open("fib_dp.csv");
+        myfile << "Num,Time" << endl;
+    }
+    else {
+        cout << "Num,Time" << endl;
+    }
+
+    //testingSuite(5);
+
+    for (int i = 0; i <= maxFib; i++) {
+        testingSuite(i);
+    }
+    
+}
 
 long long int fibonacci(int n) 
 {
@@ -19,20 +67,20 @@ long long int fibonacci(int n)
     return fib[n];
 }
 
-int main () 
-{
-    int n;
+void testingSuite(long long n) {
+    time_t start,end;
+    time(&start);
+    fibonacci(n);
+    time(&end);
+    double time_taken = double(end - start);
 
-    // Currently only supports up to the 92nd term before going out of bounds
-    std::cout << "Enter number of terms: "; std::cin >>n;
-
-    auto start = std::chrono::high_resolution_clock::now(); // Starts clock
-
-    std::cout << n<<" Fibonacci Term: "<<fibonacci(n)<<std::endl;
-
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); // Time elapsed
-
-
-    std::cout << "Program finished in " << duration.count() << " microseconds" << std::endl;
+    if(printTOCSV) {
+        std::ofstream myfile;
+        myfile.open("fib_dp.csv", std::ios_base::app);
+        myfile << n << "," << time_taken << setprecision(10) << endl;
+    }
+    else {
+        cout << n << "," << time_taken << setprecision(5) << endl;
+    }
+    
 }
--- matrix_fib.cpp
+++ matrix_fib.cpp
@@ -8,6 +8,15 @@
 */
 #include <iostream>
 #include <chrono>
+#include <iostream>
+#include <cassert>
+#include <iomanip>
+#include <ctime>
+#include <fstream>
+
+bool printTOCSV = true;
+long long maxFib = 90;
+using namespace std;
 
 // Calculates dot product of rows and column
 void matrix_multiplication(long long int F[2][2], long long int M[2][2])
@@ -48,6 +57,24 @@ int fibonacci_matrix(int n)
     return F[0][0];
 }
 
+void testingSuite(long long n) {
+    time_t start,end;
+    time(&start);
+    fibonacci_matrix(n);
+    time(&end);
+    double time_taken = double(end - start);
+
+    if(printTOCSV) {
+        std::ofstream myfile;
+        myfile.open("fib_matrix.csv", std::ios_base::app);
+        myfile << n << "," << time_taken << setprecision(5) << endl;
+    }
+    else {
+        cout << n << "," << time_taken << setprecision(5) << endl;
+    }
+    
+}
+
  
 int main()
 {
@@ -64,5 +91,18 @@ int main()
 
 
     std::cout << "Program finished in " << duration.count() << " microseconds" << std::endl;
+
+    if (printTOCSV) {
+        std::ofstream myfile;
+        myfile.open("fib_matrix.csv");
+        myfile << "Num,Time" << endl;
+    }
+    else {
+        cout << "Num,Time" << endl;
+    }
+
+    for (int i = 0; i <= maxFib; i++) {
+        testingSuite(i);
+    }
 }
  
